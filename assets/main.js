613f61c8fc9cf40a47bee35b933eb323d7507175
csvernon <47064599+csvernon@users.noreply.github.com>
2019-03-21 02:51:47 -0400

    Update main.js
    Display restaurants in results-view div

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,4 @@
+var howManyResults = 5;
 
 
 function runZomato() {
@@ -11,8 +12,13 @@ function runZomato() {
     $.ajax({
     url: queryURL,
     method: "GET",
-    header: {'user-key' : '930bc5c593df51586e7bff08f89be982'}
+    headers: {'user-key' : '930bc5c593df51586e7bff08f89be982'}
     }).then(function(response) {
         console.log(JSON.stringify(response))
-    });
-}
+        restaurantsArray = response.restaurants
+    for (i=0;i<howManyResults;i++){  
+        var currentRestaurant = restaurantsArray[i].restaurant.name
+        $("#results-view").append("Restaurant Name: " + currentRestaurant);
+        $("#results-view").append("<br></br>");
+}})};
+runZomato();
