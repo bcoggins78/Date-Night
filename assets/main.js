80dd57307c4ba451df0409d1ce99630b2ece18ea
yevheniia <jene4ka1818@gmail.com>
2019-03-20 23:18:05 -0400

    some UI added

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,10 @@
-google-ajax
+//google-ajax
 $.ajax({url:'https://api.ipgeolocation.io/ipgeo?apiKey=c182e885b4554c5fadb3366dbc198f3d&ip=1.1.1.1&fields=city', method: 'GET'}).then(function(resp){
     console.log(resp)
     console.log(resp.city)
+    $('#location').text(resp.city)
+}).then(function(response){
+    
 })
 
 function runZomato() {
