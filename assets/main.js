50ea87cf044bbeaf148333096f01c051bd13e745
csvernon <csvernon@ncsu.edu>
2019-03-20 21:46:26 -0400

    attempt 2 to do this ish

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+function runZomato() {
+
+    var latitude = "35.791470"
+    var longitude = "-78.781143";
+    var radius = "5000M"
+    var queryURL = "https://developers.zomato.com/api/v2.1/search?" + "lat=" + latitude + "&lon=" + longitude + "&radius=" + radius;
+    
+
+    $.ajax({
+    url: queryURL,
+    method: "GET",
+    header: {'user-key' : '930bc5c593df51586e7bff08f89be982'}
+    }).then(function(response) {
+        console.log(JSON.stringify(response))
+    });
+}
