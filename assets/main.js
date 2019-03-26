aa66742cba862da9e4dfe5216dd0bf4595786dc4
yevheniia <jene4ka1818@gmail.com>
2019-03-26 14:44:50 -0400

    Create Account and Login buttons, functionality added. Added button to close register, login form. Changed background and some more UI

--- assets/main.js
+++ assets/main.js
@@ -141,6 +141,28 @@ function fillModal(){
 function displayLogIn(){
   document.getElementById('loginPg').style.display = "block";
 }
+function displayRegisterForm(){
+  document.getElementById('registerPg').style.display =  "block";
+}
+function displayMovieTable(){
+  document.getElementById('movieCard').style.display = 'block';
+}
+function loginRegisterVisibility(){
+  if (('loginPg').display = 'block'){
+    ('registerPg').display = 'none'
+  }
+}
+function loginRegisterClose(){
+  document.getElementById('loginPg').style.display = "none";
+  document.getElementById('registerPg').style.display =  "none";
+}
+function registerLoginVisibility(){
+  if (('registerPg').display='block'){
+    ('loginPg').display = 'none';
+  }
+}
+loginRegisterVisibility()
+
 /////// still working on this animation //////////         
 /*$(document).on('mouseover','.poster',function(){
   console.log("animate")
@@ -151,6 +173,9 @@ $(document).on('mouseleave','.poster', function(){
     $(this).animate({width: 148 }, 2000)
 })
 */
+$(document).on('click', '.close', loginRegisterClose)
+$(document).on('click', '#registerBtn', displayRegisterForm)
+$(document).on("click", "#find-theater", displayMovieTable);
 $(document).on('click', '#loginBtn', displayLogIn )
 $(document).on("click", ".poster", fillModal);
 $(document).on("click", "#find-theater", runToday);
